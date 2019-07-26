d48d71f8a5cc7e41feaceba2e8d41b0cb24c90f9
Daniel Hara Tua Samosir <danielharatuasamosir@Daniels-MacBook-Pro.local>
2019-07-26 16:37:18 +0700

    Update header function and make new struct type

--- app/domain/service/tracking_service.go
+++ app/domain/service/tracking_service.go
@@ -6,13 +6,15 @@ import (
 	"go-event-source/app/domain/model"
 )
 
-func recordArrival(ship *model.Ship, port string) {
+type TrackingService struct{}
+
+func (self *TrackingService) RecordArrival(ship *model.Ship, port string) {
 	ship.Location = port
 	arrivalShipping := event.ArrivalShipping{Occured: "12-12-21", Recorded: "Today", Ship: ship, Port: port}
 	fmt.Println(arrivalShipping)
 }
 
-func recordDeparture(ship *model.Ship, port string) {
+func (self *TrackingService) RecordDeparture(ship *model.Ship, port string) {
 	ship.Location = port
 	departureShipping := event.DepartureShipping{Occured: "12-12-21", Recorded: "Today", Ship: ship, Port: port}
 	fmt.Println(departureShipping)
